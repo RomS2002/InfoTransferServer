fa5554d2ce753ca35801a6304d2516a17172f195
RomS2002 <romsyt2002@gmail.com>
2025-04-13 17:11:12 +0300

    First commit

---
+++ src/main/java/ru/roms2002/infotransfer/InfoTransferServerApplication.java
@@ -0,0 +1,13 @@
+package ru.roms2002.infotransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoTransferServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoTransferServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/roms2002/infotransfer/InfoTransferServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.roms2002.infotransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoTransferServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
