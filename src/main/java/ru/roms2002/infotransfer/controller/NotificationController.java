8ea542d1d115baf8b27c1c36cf1b43965d640fa7
RomS <romsyt2002@gmail.com>
2025-05-06 12:11:10 +0300

    Передача уведомлений от админ-панели серверу мессенджера

--- src/main/java/ru/roms2002/infotransfer/controller/NotificationController.java
+++ src/main/java/ru/roms2002/infotransfer/controller/NotificationController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import ru.roms2002.infotransfer.dto.ChangeDepartmentDTO;
+import ru.roms2002.infotransfer.dto.ChangeRoleDTO;
+import ru.roms2002.infotransfer.dto.ChangeStudgroupDTO;
+
 @RestController
 @RequestMapping("/notification")
 public class NotificationController {
@@ -25,5 +29,36 @@ public class NotificationController {
 	public void sendBlockedNotification(@RequestBody Integer userId) {
 		restClient.post().uri(messengerUrl + "/notification/blocked")
 				.contentType(MediaType.APPLICATION_JSON).body(userId).retrieve().toBodilessEntity();
+	}
+
+	@PostMapping("/new-group")
+	public void sendNewGroupNotification(@RequestBody String groupName) {
+		restClient.post().uri(messengerUrl + "/notification/new-group")
+				.contentType(MediaType.APPLICATION_JSON).body(groupName).retrieve()
+				.toBodilessEntity();
+	}
+
+	@PostMapping("/delete-user")
+	public void sendDeleteUserNotification(@RequestBody Integer userId) {
+		restClient.post().uri(messengerUrl + "/notification/delete-user")
+				.contentType(MediaType.APPLICATION_JSON).body(userId).retrieve().toBodilessEntity();
+	}
+
+	@PostMapping("/change-group")
+	public void sendChangeGroupNotification(@RequestBody ChangeStudgroupDTO dto) {
+		restClient.post().uri(messengerUrl + "/notification/change-group")
+				.contentType(MediaType.APPLICATION_JSON).body(dto).retrieve().toBodilessEntity();
+	}
+
+	@PostMapping("/change-role")
+	public void sendChangeRoleNotification(@RequestBody ChangeRoleDTO dto) {
+		restClient.post().uri(messengerUrl + "/notification/change-role")
+				.contentType(MediaType.APPLICATION_JSON).body(dto).retrieve().toBodilessEntity();
+	}
+
+	@PostMapping("/change-department")
+	public void sendChangeDepartmentNotification(@RequestBody ChangeDepartmentDTO dto) {
+		restClient.post().uri(messengerUrl + "/notification/change-department")
+				.contentType(MediaType.APPLICATION_JSON).body(dto).retrieve().toBodilessEntity();
 	}
 }
